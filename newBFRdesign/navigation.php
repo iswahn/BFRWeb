f05747a997185edcab43b7ecd12387eef0a74777
Ian Swahn <ianswahn@gmail.com>
2014-05-14 21:12:30 -0400

    removed newBFR designation on local machine

--- newBFRdesign/navigation.php
+++
@@ -1,9 +0,0 @@
-<div id="navigation">
-	<ul id="main_nav">
-		<li><a href="index.php" <?php if($page == "Home") echo"class=\"active\""?>>| Home |</a></li>
-		<li><a href="featured.php" <?php if($page == "Featured Listings") echo"class=\"active\""?>>| Featured Listings |</a></li>
-		<li><a href="listings.php" <?php if($page == "Listings") echo"class=\"active\""?>>| Listings |</a></li>
-		<li><a href="area.php" <?php if($page == "Area Info") echo"class=\"active\""?>>| Area Information |</a></li>
-		<li><a href="contact.php" <?php if($page == "Contact Us") echo"class=\"active\""?>>| Contact Us |</a></li>
-	</ul>
-</div>
